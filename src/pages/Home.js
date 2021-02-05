abcf546dc003df6931cba367470cbf91537e6976
karantondare <karantondare.study@gmail.com>
2021-02-06 04:07:24 +0530

    changes to grid size

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -110,7 +110,7 @@ const GameList = styled(motion.div)`
 const Games = styled(motion.div)`
   min-height: 80vh;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
   grid-column-gap: 3rem;
   grid-row-gap: 5rem;
 `;
